cd9733e8409156cca482f49ba8f81d787cdf8523
SapraG <gmv4tgyt55@gmail.com>
2022-12-12 13:17:35 +0500

    first

---
+++ index.php
@@ -0,0 +1,102 @@
+<!-- Сапрыгин Вадим -->
+<?php
+// 1 задание 
+// for ($i=0; $i < 10; $i++){
+// $arr[$i] = rand(35,42);
+// };
+// echo"<pre>";
+// print_r($arr);
+// echo"<pre>";
+// 2 задание
+// for ($i=0; $i < 10; $i++){
+// $arr[$i] = rand(35,42);
+// };
+// echo"<pre>";
+// print_r(implode("; ",$arr));
+// echo"<pre>";
+// 3 
+// for ($i=0; $i < 10; $i++){
+//     if($i!==$i++){
+//     $arr[$i] = rand(35,42);
+// }};
+// echo"<pre>";
+// print_r($arr);
+// echo"<pre>";
+// 4
+// for ($i=0; $i < 10; $i++){
+//     $arr[$i] = rand(35,42)*3;
+// };
+// echo"<pre>";
+// print_r($arr);
+// echo"<pre>";
+// 5
+// $arrRange = range(-50,50,8);
+// echo"<pre>";
+// shuffle($arrRange);
+// print_r($arrRange);
+// echo"<pre>";
+// 6
+// for ($i=0; $i < 10; $i++){
+//     $arr[$i] = rand(35,42)*3;
+// };
+// $res = array_reduce($arr,function($carry,$item){
+//     if($item%2==0)
+//         $carry+=$item;
+//         return $carry;
+// });
+// $arrend = array_sum($arr);
+// echo ($arrend);
+// echo"<br>";
+// 7
+// for ($i=0; $i < 10; $i++){
+//     $arr[$i] = rand(35,42);
+// };
+// $res = array_reduce($arr,function($carry,$item){
+//     if($item%3==0 && $item%5==0)
+//         $carry+=$item;
+//         return $carry;
+// });
+// $arrend = array_sum($arr);
+// echo ($arrend);
+// echo"<br>";
+// 8
+// for ($i=0; $i < 10; $i++){
+//     $arrRandom[$i] = rand(35,42);
+//     };
+//     $arrRange = range(-50,50,8);
+//     shuffle($arrRange);
+//     $res = array_merge($arrRange,$arrRandom);
+// 9
+// for ($i=0; $i < 10; $i++){
+// $arrRandom[$i] = rand(35,42);
+// };
+// $arrRange = range(-50,50,8);
+// shuffle($arrRange);
+// echo"<pre>";
+// $res = array_merge($arrRange,$arrRandom);
+// echo (min($res));
+// echo"<br>";
+// 10
+// for ($i=0; $i < 10; $i++){
+//     $arr[$i] = rand(35,42);
+// };
+// foreach($arr as $item){
+//     if($item==5){
+//         $item =1000;
+//     }
+//     elseif($item==6){
+//         $item= 2000;
+//     }
+// };
+// echo"<pre>";
+// print_r ($arr);
+// echo"<pre>";
+// 12
+// for ($i=0; $i < 10; $i++){
+// $arr[$i] = rand(35,42);
+// };
+// echo"<pre>";
+// $res = array_pop($arr);
+// print_r($arr);
+// echo"<pre>";
+?>
